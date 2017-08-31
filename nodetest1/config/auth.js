e5a3beefb8c4d7eaea69248d038743773b9f3c5b
gurpreet <yourfree93@gmail.com>
2017-08-31 18:05:48 +0530

    removing nodetest1 folder

--- nodetest1/config/auth.js
+++
@@ -1,9 +0,0 @@
-//config.js
-module.exports={
-  'googleAuth' : {
-          'clientID'      : '104141969179-enrbci3mos17edgvs3cro0ogjh133ng5.apps.googleusercontent.com',
-          'clientSecret'  : 'S4ARoCuC4U-NZuo9I2BDA4lD',
-          'callbackURL'   : 'http://localhost:3000/auth/google/callback'
-      }
-
-}
